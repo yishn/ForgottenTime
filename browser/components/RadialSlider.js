42af9f9a33557fdcd9766bbc05861fb82f518496
Yichuan Shen <shen.yichuan@gmail.com>
2017-02-13 17:15:51 +0100

    Triangle indicator as HTML element

--- browser/components/RadialSlider.js
+++ browser/components/RadialSlider.js
@@ -1,17 +1,10 @@
 const {h, Component} = require('preact')
 
-class RadialSlider extends Component {
-    render() {
-        return h('div', {class: 'radial-slider'},
-            h('svg', {width: 116, height: 116},
-                // Outer circle
-                h('circle', {cx: 58, cy: 58, r: 58, fill: '#338AF4'}),
-
-                // Inner circle
-                h('circle', {cx: 58, cy: 58, r: 54, fill: '#282C34'})
-            )
-        )
-    }
-}
-
-module.exports = RadialSlider
+module.exports = ({value}) => h('div', {class: 'radial-slider'},
+    h('svg', {width: 138, height: 138},
+        h('circle', {cx: 68, cy: 68, r: 60, fill: 'rgba(40, 44, 52, .5)'}),
+        h('circle', {cx: 68, cy: 68, r: 60, fill: '#338AF4'})
+    ),
+    h('div', {class: 'indicator'}),
+    h('div', {class: 'inner'})
+)
--- browser/components/TitleBar.js
+++ browser/components/TitleBar.js
@@ -4,6 +4,7 @@ const {h, Component} = require('preact')
 class TitleBar extends Component {
     render() {
         return h('header', {},
+            h('span', {class: 'drag'}),
             h('span', {class: 'close', onClick: () => remote.getCurrentWindow().close()},
                 h('img', {
                     src: 'img/close.svg',
