e1eb5a827b3e865de750b5c0fb289f2b61a20da3
Yichuan Shen <shen.yichuan@gmail.com>
2017-02-13 21:12:37 +0100

    Correctly center TimerDisplay

--- browser/components/App.js
+++ browser/components/App.js
@@ -3,8 +3,6 @@ const TitleBar = require('./TitleBar')
 const RadialSlider = require('./RadialSlider')
 const TimerDisplay = require('./TimerDisplay')
 
-let padZero = (n, x) => [...Array(Math.max(0, n - ('' + x).length))].map(x => '0').join('') + x
-
 class App extends Component {
     constructor() {
         super()
@@ -14,24 +12,32 @@ class App extends Component {
 
     render(props, state) {
         let minutes = Math.floor(state.remaining / 60)
-        let seconds = state.remaining - minutes * 60
         let endDate = new Date(Date.now() + minutes * 60 * 1000)
+        let getSeconds = value => Math.round(value * 60) * 60
 
         return h('div', {id: 'root'},
             h(TitleBar),
 
             h(RadialSlider, {
                 value: state.value,
+                maxValue: 16.65,
                 onInput: value => this.setState({
                     value,
-                    seconds: Math.round(value * 60) * 60,
-                    remaining: Math.round(value * 60) * 60
+                    seconds: getSeconds(value),
+                    remaining: getSeconds(value),
+                    countdown: false
+                }),
+                onSet: value => this.setState({
+                    seconds: getSeconds(value),
+                    remaining: getSeconds(value),
+                    value: Math.round(value * 60) / 60,
+                    countdown: true
                 })
             },
                 h(TimerDisplay, {
-                    above: `${endDate.getHours()}:${padZero(2, endDate.getMinutes())}`,
-                    label: `${minutes}'`,
-                    below: `${seconds}"`
+                    time: endDate,
+                    minutes,
+                    seconds: state.remaining - minutes * 60
                 })
             )
         )
--- browser/components/TimerDisplay.js
+++ browser/components/TimerDisplay.js
@@ -1,11 +1,13 @@
 const {h, Component} = require('preact')
 
+let padZero = (n, x) => [...Array(Math.max(0, n - ('' + x).length))].map(x => '0').join('') + x
+
 class TimerDisplay extends Component {
-    render({label, above, below}) {
+    render({time, minutes, seconds}) {
         return h('ul', {class: 'timer-display'},
-            h('li', {class: 'above'}, above),
-            h('li', {class: 'label'}, label),
-            h('li', {class: 'below'}, below)
+            h('li', {class: 'time'}, `${time.getHours()}:${padZero(2, time.getMinutes())}`),
+            h('li', {class: 'minutes'}, h('span', {}, "'"), `${minutes}'`),
+            h('li', {class: 'seconds'}, h('span', {}, '"'), `${seconds}"`)
         )
     }
 }
